afc8e0a4166d0c428172ec485df54c1b51493a4e
Keith <kayzee@live.com>
2019-08-01 18:33:40 -1000

    setup

---
+++ public/test/shopping-list-item.test.js
@@ -0,0 +1,8 @@
+const expect = chai.expect;
+
+describe("ShoppingListItem", function() {
+  it("should be a class", function() {
+    const item = new ShoppingListItem("apple");
+    expect(item).equal(0);
+  });
+});
